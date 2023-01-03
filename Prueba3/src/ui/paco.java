e92a8bb749fa80ee162941b2781926656bce545b
Dulce <dulce@dulce>
2023-01-03 17:08:44 -0500

    commit 1

---
+++ Prueba3/src/ui/paco.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package ui;
+
+/**
+ * @author Dulce
+ *
+ */
+public class paco {
+
+	/**
+	 * JJJJJJ
+	 */
+	public paco() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("holi");
+		
+		System.out.println();
+
+	}
+	
+	
+}
